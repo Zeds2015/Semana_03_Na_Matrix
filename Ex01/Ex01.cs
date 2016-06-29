dc6d6f558ec0611c303181e454339d137dc60204
Zeds2015 <arrudamateus3@hotmail.com>
2016-06-29 18:15:06 -0300

    Add files via upload

---
+++ Ex01/Ex01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Fazer um programa que leia três valores numéricos, determine e imprima o menor deles.
+/// </summary>
+
+namespace Ex01
+{
+    public class Ex01
+    {
+        public int Calcular(params int[] num)
+        {
+            int menor = num.Min();
+            return menor;
+        }
+    }
+}
---
+++ Ex01Test/Ex01Test.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ex01;
+
+/// <summary>
+/// Fazer um programa que leia três valores numéricos, determine e imprima o menor deles.
+/// </summary>
+
+namespace Ex01Test
+{
+    [TestClass]
+    public class Ex01Test
+    {
+        [TestMethod]
+        public void Testar_o_Menor_Numero()
+        {
+            var num = new int[3];
+            int cont = 1;
+
+            for(int i = 0; i < 3; i++,cont++)
+            {
+                num[i] = cont;
+            }
+            Ex01.Ex01 calcular_Menor_Numero = new Ex01.Ex01();
+            var retorno = calcular_Menor_Numero.Calcular(num);
+
+            Assert.AreEqual(1, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_o_Menor_Numero2()
+        {
+            var num = new int[3];
+            int cont = 3;
+
+            for (int i = 0; i < 3; i++, cont--)
+            {
+                num[i] = cont;
+            }
+            Ex01.Ex01 calcular_Menor_Numero = new Ex01.Ex01();
+            var retorno = calcular_Menor_Numero.Calcular(num);
+
+            Assert.AreEqual(1, retorno);
+        }
+
+        [TestMethod]
+        public void Testar_o_Menor_Numero3()
+        {
+            var num = new int[3];
+            int cont = 3;
+
+            for (int i = 0; i < 3; i++, cont--)
+            {
+                num[i] = cont;
+                if(i == 2)
+                {
+                    cont = 1;
+                    num[i] = cont;
+                    cont++;
+                }
+            }
+            Ex01.Ex01 calcular_Menor_Numero = new Ex01.Ex01();
+            var retorno = calcular_Menor_Numero.Calcular(num);
+
+            Assert.AreEqual(1, retorno);
+        }
+    }
+}
